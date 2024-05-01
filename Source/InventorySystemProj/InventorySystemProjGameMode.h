dc609288afbb137124415ca26a30a4a0e80f2139
FeroXx07 <59049844+FeroXx07@users.noreply.github.com>
2024-05-01 16:41:55 +0200

    Initial commit

---
+++ Source/InventorySystemProj/InventorySystemProjGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InventorySystemProjGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInventorySystemProjGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInventorySystemProjGameMode();
+};
+
+
+
